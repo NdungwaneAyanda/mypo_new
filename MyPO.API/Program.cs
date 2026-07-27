f0b8b8a15878492abfbe34a56fdfb8ce74c99301
amidelitsupport <it.support@amidel.co.za>
2026-07-27 12:01:07 +0200

    Add GitHub Actions CI/CD for API and frontend deployment V2 27-07-2026

--- MyPO.API/Program.cs
+++ MyPO.API/Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using MyPO.API.Data;
